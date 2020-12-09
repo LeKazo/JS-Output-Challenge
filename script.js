216d2ef6325173f56555f2c33914051e45ab29aa
Sawman_Quentin <Sawman_Quentin@student.mahoningctc.com>
2020-12-09 12:52:41 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Enter your code below to edit index.html
+
+yourName = "Ninjafiveo"
+document.getElementById("yourName").innerHTML = yourName;
+
+myBio = "Howdy! im Q, or Quentin. suffer with no bio :)"
+document.getElementById("myBio").innerHTML = myBio
+
+collegeChoice = "Well, i most likely would want to go to YSU, since its a great school and its very close by"
+document.getElementById("collegeChoice").innerHTML = collegeChoice
+
+
+collegeReason = "Im Choosing to go into Computer science since i have a interest in computers, and YSU since its very close by"
+document.getElementById("collegeReason").innerHTML = collegeReason
+
+careerChoice = "Software Programmer"
+document.getElementById("careerChoice").innerHTML = careerChoice
+
+companyChoice = "Im not sure"
+document.getElementById("companyChoice").innerHTML = companyChoice
+
+careerReason = "Since, i wanna be able to build websites for clients"
+document.getElementById("companyChoice").innerHTML = companyChoice
+
+answerOne = "The Document Object Model is a cross-platform and language-independent interface that treats an XML or HTML document as a tree structure wherein each node is an object representing a part of the document."
+document.getElementById("answerOne").innerHTML = answerOne
+
+answerTwo = "Writing HTML, using innerHTML, writing into a alert box, and writing into the browser console"
+document.getElementById("answerTwo").innerHTML = answerTwo
+
+answerThree = "You can create dynamic hmml inside js, it can chaange all the html attributes, elements, and css styles"
+document.getElementById("answerThree").innerHTML = answerThree
